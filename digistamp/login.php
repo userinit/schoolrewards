cb15a250d9eca0ad587745e4ab7779eeafc2b61f
BuildTools <unconfigured@null.spigotmc.org>
2024-02-11 12:40:30 +0000

    Added a bare bones dashboard and validation checks for imported CSV

--- digistamp/login.php
+++ digistamp/login.php
@@ -6,147 +6,163 @@ $srvuser = "root";
 $srvpass = "";
 $db = "schoolrewardsdb";
 
-if ($_SERVER["HTTP_REFERER"] !== "http://localhost/digistamp/login.html") {
-    http_response_code(307);
-    header("Location: http://localhost/digistamp/login.html");
-    exit();
-}
-elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Take the username and password
-    $username = htmlspecialchars($_POST["username"], ENT_QUOTES, 'UTF-8');
-    $password = $_POST["password"];
 
-    // Starts connection and ends if failed
-    $conn = new mysqli($host, $srvuser, $srvpass, $db);
-    if ($conn->connect_error) {
-        die("Connection erorr: " . $conn->connect_error);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Decode JSON then take the username and password
+    $postData = json_decode(file_get_contents('php://input'), TRUE);
+    if ($postData == null) {
+        header("Content-Type: application/json");
+        echo json_encode(array('error' => 'Failed to decode JSON data.'));
     }
     else {
-        // Prepared statement to prevent SQL injection
-        $stmt = $conn->prepare("SELECT user_role FROM roles WHERE username = ?;");
-        $stmt->bind_param("s",$username);
-        $result = $stmt->execute();
-        if (!$result) {
-            // Error handling
-        }
-        $result_set = $stmt->get_result();
-        $num_rows = $result_set->num_rows;
-        if (!($num_rows > 0)) {
-            echo "Invalid username or password";
+        $username = $postData['username'];
+        $password = $postData['password'];
+
+        // Non-AJAX method below
+        //$username = htmlspecialchars($_POST["username"], ENT_QUOTES, 'UTF-8');
+        //$password = $_POST["password"];
+
+        // Starts connection and ends if failed
+        $conn = new mysqli($host, $srvuser, $srvpass, $db);
+        if ($conn->connect_error) {
+            die("Connection erorr: " . $conn->connect_error);
         }
         else {
-            // Statements depending on whether person is staff or not (staff being teacher/admin)
-            $row = $result_set->fetch_assoc();
-            $role = $row['user_role'];
-            if ($role === "admin" || $role === "teacher") {
-                $stmt = $conn->prepare("SELECT * FROM staff WHERE username = ?;");
-            }
-            else {
-                $stmt = $conn->prepare("SELECT * FROM students WHERE username = ?;");
-            }
+            // Prepared statement to prevent SQL injection
+            $stmt = $conn->prepare("SELECT user_role FROM roles WHERE username = ?;");
             $stmt->bind_param("s",$username);
             $result = $stmt->execute();
             if (!$result) {
-                // error handling
+                // Error handling
             }
             $result_set = $stmt->get_result();
             $num_rows = $result_set->num_rows;
             if (!($num_rows > 0)) {
-                echo "Invalid username or password";
+                $response = json_encode(array('invalid' => 'Invalid username or password.'));
+                header("Content-Type: application/json");
+                echo $response;
             }
             else {
+                // Statements depending on whether person is staff or not (staff being teacher/admin)
                 $row = $result_set->fetch_assoc();
-                $dbuser = $row['username'];
-                $dbpass = $row['password_hash'];
-                if ($role === "admin" || $role === "teacher" || $role === "student") {
-                    $_SESSION['role'] = $role;
-                }
-                if (!password_verify($password, $dbpass)) {
-                    echo "Invalid username or password";
+                $role = $row['user_role'];
+                if ($role === "admin" || $role === "teacher") {
+                    $stmt = $conn->prepare("SELECT * FROM staff WHERE username = ?;");
                 }
                 else {
-                    // Start session with their username
-                    $_SESSION['username'] = $username;
-
-                    // DEFINE FUNCTION VALUES
-
-                    // NOTE: THESE TOKENS ARE BY NO MEANS ADEQUATE FOR CSRF PROTECTION!!!!!
-                    // IF YOU WANT TO DO CSRF PROTECTION EITHER SEND TOKEN TO FRONT END OR USE JWT!!!
-                    ini_set('session.cookie_lifetime', 3600);
-                    session_start();
-
-                    function generate_csrf_token() {
-                        $token = bin2hex(random_bytes(32));
-                        $expirationTime = time() + 3600; // 24 hour token (3600=24x60x60)
-                        $_SESSION['csrf_token'] = $token;
-                        $_SESSION['token_expiration'] = $expirationTime;
-                        return['token' => $token, 'expiration' => $expirationTime];
+                    $stmt = $conn->prepare("SELECT * FROM students WHERE username = ?;");
+                }
+                $stmt->bind_param("s",$username);
+                $result = $stmt->execute();
+                if (!$result) {
+                    // error handling
+                }
+                $result_set = $stmt->get_result();
+                $num_rows = $result_set->num_rows;
+                if (!($num_rows > 0)) {
+                    $response = json_encode(array('invalid' => 'Invalid username or password.'));
+                    header("Content-Type: application/json");
+                    echo $response;
+                }
+                else {
+                    $row = $result_set->fetch_assoc();
+                    $dbuser = $row['username'];
+                    $dbpass = $row['password_hash'];
+                    if ($role === "admin" || $role === "teacher" || $role === "student") {
+                        $_SESSION['role'] = $role;
                     }
-
-                    function store_csrf_token($host, $srvuser, $srvpass, $db, $username, $token, $expirationTime) {
-                        $conn = new mysqli($host, $srvuser, $srvpass, $db);
-                        if ($conn->connect_error) {
-                            die("Connection error: " . $conn->connect_error);
-                        }
-                        else {
-                            $stmt = $conn->prepare("INSERT INTO tokens (username, token, expiration_time) VALUES (?, ?, ?);");
-                            $stmt->bind_param("ssi", $username, $token, $expirationTime);
-                            $stmt->execute();
-                            $stmt->close();
-                            $conn->close();
-                        }
+                    if (!password_verify($password, $dbpass)) {
+                        $response = json_encode(array('invalid' => 'Invalid username or password.'));
+                        header("Content-Type: application/json");
+                        echo $response;
                     }
+                    else {
+                        // Start session with their username
+                        $_SESSION['username'] = $username;
 
-                    // Adds CSRF token to db
-                    function set_csrf_token($host, $srvuser, $srvpass, $db, $username) {
-                        $conn = new mysqli($host, $srvuser, $srvpass, $db);
-                        if ($conn->connect_error) {
-                            die("Connection error: " . $conn->connect_error);
+                        // DEFINE FUNCTION VALUES
+
+                        // NOTE: THESE TOKENS ARE BY NO MEANS ADEQUATE FOR CSRF PROTECTION!!!!!
+                        // IF YOU WANT TO DO CSRF PROTECTION EITHER SEND TOKEN TO FRONT END OR USE JWT!!!
+                        ini_set('session.cookie_lifetime', 3600);
+                        session_start();
+
+                        function generate_csrf_token() {
+                            $token = bin2hex(random_bytes(32));
+                            $expirationTime = time() + 3600; // 24 hour token (3600=24x60x60)
+                            $_SESSION['csrf_token'] = $token;
+                            $_SESSION['token_expiration'] = $expirationTime;
+                            return['token' => $token, 'expiration' => $expirationTime];
                         }
-                        else {
-                            $stmt = $conn->prepare("SELECT * FROM tokens WHERE username = ?;");
-                            $stmt->bind_param("s", $username);
-                            $result = $stmt->execute();
-                            if (!$result) {
-                                // error handling
-                            }
 
-                            $result_set = $stmt->get_result();
-                            $num_rows = $result_set->num_rows;
-                            if (!($num_rows > 0)) {
-                                //$row = $result_set->fetch_assoc();
-                                //$expirationTime = $row['expiration_time'];
-                                $stmt->close();
-                                $conn->close();
-                                $tokenInfo = generate_csrf_token();
-                                $token = $tokenInfo['token'];
-                                $expirationTime = $tokenInfo['expiration'];
-                                store_csrf_token($host, $srvuser, $srvpass, $db, $username, $token, $expirationTime);
+                        function store_csrf_token($host, $srvuser, $srvpass, $db, $username, $token, $expirationTime) {
+                            $conn = new mysqli($host, $srvuser, $srvpass, $db);
+                            if ($conn->connect_error) {
+                                die("Connection error: " . $conn->connect_error);
                             }
                             else {
-                                $tokenInfo = generate_csrf_token();
-                                $token = $tokenInfo['token'];
-                                $expirationTime = $tokenInfo['expiration'];
-                                $stmt = $conn->prepare("UPDATE tokens SET token = ?, expiration_time = ? WHERE username = ?;");
-                                $stmt->bind_param("sis", $token, $expirationTime, $username);
+                                $stmt = $conn->prepare("INSERT INTO tokens (username, token, expiration_time) VALUES (?, ?, ?);");
+                                $stmt->bind_param("ssi", $username, $token, $expirationTime);
                                 $stmt->execute();
                                 $stmt->close();
                                 $conn->close();
                             }
                         }
-                    }
 
-                    // TOKEN LOGIC
-                    // Token deprecated - just use $_SESSION['username']
-                    set_csrf_token($host, $srvuser, $srvpass, $db, $username);
-                    
-                    // redirect here
-                    //http_response_code(302);
-                    header("Location: http://localhost/digistamp/dashboard");
+                        // Adds CSRF token to db
+                        function set_csrf_token($host, $srvuser, $srvpass, $db, $username) {
+                            $conn = new mysqli($host, $srvuser, $srvpass, $db);
+                            if ($conn->connect_error) {
+                                die("Connection error: " . $conn->connect_error);
+                            }
+                            else {
+                                $stmt = $conn->prepare("SELECT * FROM tokens WHERE username = ?;");
+                                $stmt->bind_param("s", $username);
+                                $result = $stmt->execute();
+                                if (!$result) {
+                                    // error handling
+                                }
+
+                                $result_set = $stmt->get_result();
+                                $num_rows = $result_set->num_rows;
+                                if (!($num_rows > 0)) {
+                                    //$row = $result_set->fetch_assoc();
+                                    //$expirationTime = $row['expiration_time'];
+                                    $stmt->close();
+                                    $conn->close();
+                                    $tokenInfo = generate_csrf_token();
+                                    $token = $tokenInfo['token'];
+                                    $expirationTime = $tokenInfo['expiration'];
+                                    store_csrf_token($host, $srvuser, $srvpass, $db, $username, $token, $expirationTime);
+                                }
+                                else {
+                                    $tokenInfo = generate_csrf_token();
+                                    $token = $tokenInfo['token'];
+                                    $expirationTime = $tokenInfo['expiration'];
+                                    $stmt = $conn->prepare("UPDATE tokens SET token = ?, expiration_time = ? WHERE username = ?;");
+                                    $stmt->bind_param("sis", $token, $expirationTime, $username);
+                                    $stmt->execute();
+                                    $stmt->close();
+                                    $conn->close();
+                                }
+                            }
+                        }
+
+                        // TOKEN LOGIC
+                        // Token deprecated - just use $_SESSION['username']
+                        set_csrf_token($host, $srvuser, $srvpass, $db, $username);
+                        
+                        // redirect here
+                        //http_response_code(302);
+                        header("Location: http://localhost/digistamp/dashboard");
+                    }
                 }
             }
         }
     }
+}
+else {
+    http_response_code(405);
 }
 
 
--- digistamp/new-users.php
+++ digistamp/new-users.php
@@ -6,6 +6,30 @@ $srvuser = "root";
 $srvpass = "";
 $db = "schoolrewardsdb";
 
+// at start everything is invalid
+$validClass = FALSE;
+$validTutor = FALSE;
+$noWhitespaces = FALSE;
+
+// assigning classes and tutors - update as needed
+$yearInfo = [
+    "Year 10" => [
+        "Tutors" => ["Newton", "Einstein", "Bohr", "Darwin", "Pasteur"],
+        "Classes" => ["Freud", "Galilei", "Lavoisier", "Kepler", "Copernicus"]
+    ],
+    "Year 11" => [
+        "Tutors" => ["Faraday", "Maxwell", "Bernard", "Boas", "Heisenberg"],
+        "Classes" => ["Pauling", "Virchow", "Schrodinger", "Rutherford", "Dirac"]
+    ],
+    "Year 12" => [
+        "Tutors" => ["Vesalius", "Brahe", "Buffon", "Boltzmann", "Planck"],
+        "Classes" => ["Curie", "Herschel", "Lyell", "Lapalace", "Hubble"]
+    ],
+    "Year 13" => [
+        "Tutors" => ["Thomson", "Born", "Crick", "Fermi", "Liebig"],
+        "Classes" => ["Eddington", "Harvey", "Malpighi", "Huygens", "Gauss"]
+    ]];
+
 if (isset($_SESSION['role']) && $_SESSION['role'] === "admin") {
     if ($_SERVER['REQUEST_METHOD'] === "POST") {
         if (isset($_FILES['file']) && $_FILES['file']['error'] == UPLOAD_ERR_OK) {
@@ -26,39 +50,70 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === "admin") {
                     // student logic
                     echo "<h2>Uploaded Student Records:</h2>";
                     echo "<table border='1'>";
-                    echo "<tr><th>Name</th><th>Year</th><th>Tutor Group</th><th>Class</th><th>Username</th><th>Password</th><th>Stamps</th></tr>";
+                    echo "<tr><th>Surname</th><th>Forename</th><th>Year</th><th>Tutor</th><th>Class</th><th>Password</th></tr>";//<th>Stamps</th></tr>";
                     // while loop iterates until there are no more lines left
                     while (($data = fgetcsv($handle)) !== FALSE) {
-                        $name = $data[0];
-                        $year = $data[1];
-                        $tutor = $data[2];
-                        $class = $data[3];
-                        $username = $data[4];
-                        $password = $data[5];
-                        $stamps = $data[6]; // Stamps not at 0 because it's mid year
-                        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+                        $surname = trim($data[0]);
+                        $forename = trim($data[1]);
+                        $year = trim($data[2]);
+                        $tutor = trim($data[3]);
+                        $class = trim($data[4]);
+                        $password = trim($data[5]);
+                        // Stamp score could be mid-year but it will be 0 until further notice.
+                        $stamps = 0;
+                        // $stamps = $data[6]; // Stamps not at 0 because it's mid year
 
-                        if (is_int(intval($year)) && is_int(intval($stamps))) {
+                        if (!preg_match('/\s/', $password)) {
+                            $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+                            $noWhitespaces = TRUE;
+                        }
+                        if (is_int(intval($year)) && is_int(intval($stamps)) && preg_match("/^[a-zA-Z' -]+$/", $surname) && preg_match("/^[a-zA-Z' -]+$/", $forename && preg_match("/^[a-zA-Z]/", $surname) && preg_match("/^[a-zA-Z]/", $forename))) {
                             $year = (int)$year;
-                            $stamps = (int)$stamps;
+                            $textYear = "Year " . $year;
+                            if (isset($yearInfo[$textYear])) {
+                                foreach ($yearInfo[$textYear]['Classes'] as $arrayClass) {
+                                    if ($class === $arrayClass) {
+                                        $validClass = TRUE;
+                                        break;
+                                    }
+                                }
+                                foreach ($yearInfo[$textYear]['Tutors'] as $arrayTutor) {
+                                    if ($tutor === $arrayTutor) {
+                                        $validTutor = TRUE;
+                                        break;
+                                    }
+                                }
+                            }
+                            if ($validClass && $validTutor && $noWhitespaces) {
+                                // makes username prefix i.e. the 19 in 19surname.initial (11-6=5, 24-5=19)
+                                $currentYear = date('y'); // 2 digit year number - i.e. for 2024, you get 24
+                                $howLongAgo = (int)$year - 6; // to see how long ago they joined secondary. 6 because 6 years in primary
+                                $prefix = $currentYear - $howLongAgo; // calculates the year they joined secondary.
 
-                            // Outputs current record from CSV
-                            echo "<tr><td>$name</td><td>$year</td><td>$tutor</td><td>$class</td><td>$username</td><td>$password</td><td>$stamps</td></tr>";
-                            // Adds them to students table
-                            $stmt = prepare("INSERT INTO students (fullname, school_year, tutor, class, username, hashed_password, stamps) VALUES (?,?,?,?,?,?);");
-                            $stmt->bind_param("sissssi", $name, $year, $tutor, $class, $username, $hashed_password, $stamps);
-                            $stmt->execute();
-                            $stmt->close();
-                            // Adds them to roles table
-                            $stmt = prepare("INSERT INTO roles (username, user_role) VALUES (?, ?);");
-                            $stmt->bind_param("ss", $username, "student");
-                            $stmt->execute();
-                            $stmt->close();
+                                // For loop to remove 
+                                $username = $prefix . $surname . $forename[0];
+                                //$stamps = (int)$stamps;
+
+                                // Outputs current record from CSV
+                                echo "<tr><td>$surname</td><td>$forename</td><td>$username</td> <td>$year</td><td>$tutor</td><td>$class</td><td>$username</td><td>$password</td></tr>";
+                                // Adds them to students table
+                                $stmt = prepare("INSERT INTO students (forename, surname, school_year, tutor, class, username, hashed_password, stamps) VALUES (?,?,?,?,?,?,?,?);");
+                                $stmt->bind_param("ssissssi", $forename, $surname, $year, $tutor, $class, $username, $hashed_password, $stamps);
+                                $stmt->execute();
+                                $stmt->close();
+                                // Adds them to roles table
+                                $stmt = prepare("INSERT INTO roles (username, user_role) VALUES (?, ?);");
+                                $stmt->bind_param("ss", $username, "student");
+                                $stmt->execute();
+                                $stmt->close();
+                            }
                         }
                     }
+                
                     $conn->close();
                     
                 }
+                
                 else {
                     // staff logic
                     echo "<h2>Uploaded Staff Records:</h2>";
