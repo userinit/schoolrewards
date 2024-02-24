a137d5c1337499dd14a96dd7e10be2f6562ee359
userinit <93477626+userinit@users.noreply.github.com>
2024-02-24 22:59:53 +0000

    Added password reset in settings menu

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,10 @@ $srvuser = "root";
 $srvpass = "";
 $db = "schoolrewardsdb";
 
+// For password reset
+$minChars = 10; // Edit this depending on password requirements
+$maxChars = 30; // Max chars to stop overflows
+
 session_start();
 
 if (isset($_SESSION['username']) && isset($_SESSION['role']) && $_SESSION['role'] === "student") {
@@ -72,17 +76,6 @@ if (isset($_SESSION['username']) && isset($_SESSION['role']) && $_SESSION['role'
             }
         }
     }
-    elseif ($_SERVER['REQUEST_METHOD'] === "POST") {
-        $username = $_SESSION['username'];
-        $jsonData = file_get_contents('php://input');
-        // decodes request
-        $decoded = json_decode($jsonData, true);
-        if (array_key_exists('logout', $decoded) && $decoded['logout'] === TRUE) {
-            header("Location: http://localhost/digistamp/login.html");
-            $_SESSION = [];
-            session_destroy();
-        }
-    }
 }
 elseif (isset($_SESSION['username']) && isset($_SESSION['role']) && $_SESSION['role'] === "teacher") {
     if ($_SERVER['REQUEST_METHOD'] === "GET" && isset($_GET['item']) && $_GET['item'] === "profile") {
@@ -112,6 +105,133 @@ elseif (isset($_SESSION['username']) && isset($_SESSION['role']) && $_SESSION['r
         $stmt->close();
         $conn->close();
         http_response_code(200);
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $roleArray = ['student', 'teacher', 'admin'];
+    if (isset($_SESSION['username']) && isset($_SESSION['role']) && in_array($_SESSION['role'], $roleArray)) {
+        $username = $_SESSION['username'];
+        $jsonData = file_get_contents('php://input');
+        // decodes request
+        $decoded = json_decode($jsonData, true);
+        if (array_key_exists('logout', $decoded) && $decoded['logout'] === TRUE) {
+            header("Location: http://localhost/digistamp/login.html");
+            $_SESSION = [];
+            session_destroy();
+        }
+        elseif (array_key_exists("old", $decoded) && array_key_exists("new", $decoded) && array_key_exists("confirm", $decoded)) {
+            // defines bools for validation
+            $hasUpper = FALSE;
+            $hasLower = FALSE;
+            $hasNums = FALSE;
+            $hasSyms = FALSE;
+            $goodLength = FALSE;
+            
+            $oldPass = $decoded['old'];
+            $newPass = $decoded['new'];
+            $confPass = $decoded['confirm'];
+            $conn = new mysqli($host, $srvuser, $srvpass, $db);
+            if ($conn->connect_error) {
+                die("Error: " . $conn->connect_error);
+            }
+            if ($_SESSION['role'] === "teacher" || $_SESSION['role'] === "admin") {
+                $stmt = $conn->prepare("SELECT * FROM staff WHERE username = ?;");
+            }
+            else {
+                $stmt = $conn->prepare("SELECT * FROM students WHERE username = ?;");
+            }
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if ($result->num_rows > 0) {
+                // User exists
+                $results = $result->fetch_assoc();
+                $hash = $results['password_hash'];
+                if (password_verify($oldPass, $hash)) {
+                    // Old password matches, check whether passwords are the same then change them
+                    if ($newPass === $confPass) {
+                        // Make sure they are not trying to change to the same password they already have
+                        if ($oldPass !== $newPass) {
+                            // Check nums
+                            if (preg_match('/[0-9]/', $newPass)) {
+                                $hasNums = TRUE;
+                            }
+                            if (preg_match('/[a-z]/', $newPass)) {
+                                $hasLower = TRUE;
+                            }
+                            if (preg_match('/[A-Z]/', $newPass)) {
+                                $hasUpper = TRUE;
+                            }
+                            if (preg_match("/[!£$%^&*()_+|~=`{}\[\]:\";'<>?,.\/-]/", $newPass)) {
+                                $hasSyms = TRUE;
+                            }
+                            if (strlen($newPass) >= $minChars && strlen($newPass) <= $maxChars) {
+                                $goodLength = TRUE;                                
+                            }
+                            if ($hasNums && $hasLower && $hasUpper && $hasSyms && $goodLength) {
+                                $newHash = password_hash($newPass, PASSWORD_BCRYPT);
+                                if ($_SESSION['role'] === "teacher" || $_SESSION['role'] === "admin") {
+                                    $stmt = $conn->prepare("UPDATE staff SET password_hash = ? WHERE username = ?;");
+                                }
+                                else {
+                                    $stmt = $conn->prepare("UPDATE students SET password_hash = ? WHERE username = ?;");
+                                }
+                                $stmt->bind_param("ss", $newHash, $username);
+                                $stmt->execute();
+                                if ($stmt->affected_rows > 0) {
+                                    $success = json_encode(array("success" => "Successfully changed password"));
+                                    header("Content-Type: application/json");
+                                    echo $success;
+                                }
+                                else {
+                                    $failure = json_encode(array("failure" => "Password change failed"));
+                                    header("Content-Type: application/json");
+                                    echo $failure;
+                                }
+                            }
+                            else {
+                                $response = '';
+                                if (!$goodLength) {
+                                    $response .= "Password needs to be between $minChars and $maxChars"; 
+                                }
+                                if (!($hasLower && $hasUpper && $hasNums && $hasSyms)) {
+                                    if (!$goodLength) {
+                                        $response .= "<br>";
+                                    }
+                                    $response .= "Password needs to have lowercase, uppercase, numbers and symbols";
+                                }
+                                $failure = json_encode(array("failure" => $response));
+                                header("Content-Type: application/json");
+                                echo $failure;
+                            }
+                        }
+                        else {
+                            $failure = json_encode(array("failure" => "Password must be different to current password"));
+                            header("Content-Type: application/json");
+                            echo $failure;
+                        }
+                    }
+                    else {
+                        $failure = json_encode(array("failure" => "Passwords need to match"));
+                        header("Content-Type: application/json");
+                        echo $failure;
+                    }
+                }
+                else {
+                    // Old password doesn't match, give response
+                    $failure = json_encode(array("failure" => "Invalid password"));
+                    header("Content-Type: application/json");
+                    echo $failure;
+                }
+            }
+            else {
+                // User doesn't exist
+                $failure = json_encode(array("failure" => "User doesn't exist"));
+                header("Content-Type: application/json");
+                echo $failure;
+            }
+        }
     }
 }
 // Add elseif for teachers then admins
--- script/dashboard.js
+++ script/dashboard.js
@@ -1,3 +1,6 @@
+const minChars = 10 // Password requirements. Change as needed
+const maxChars = 30
+
 function removeCookie(name) {
     document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
@@ -50,7 +53,7 @@ function showProfile() {
 
 document.addEventListener('DOMContentLoaded', function() {
     showProfile();
-})
+});
 
 function showStamps() {
     document.getElementById('profile').style.display = 'none';
@@ -67,7 +70,7 @@ function showStamps() {
         if (typeof data === 'object' && data !== null) {
             if ('stamps' in data) {
                 var stamps = data.stamps;
-                stampsElement = document.getElementById("stampsText");
+                var stampsElement = document.getElementById("stampsText");
                 stampsElement.innerHTML = `<p><span class='label'>Stamps: </span><span class='data'>${stamps}</span></p>`;
                 var intStamps = parseInt(stamps);
                 // Goals: Bronze (250), silver (500), gold (750), platinum (1000), diamond (1500), double diamond (3000)
@@ -125,7 +128,7 @@ function showStamps() {
                     var roundedDown = Math.floor(percentageCompletion);
 
                     // displays next goal
-                    totalStamps = parseInt(stamps);
+                    var totalStamps = parseInt(stamps);
                     stampsElement.innerHTML += `<p><span class="label">Next goal:</span> ${textGoal} (${stamps}/${goal})</p>`
                     stampsElement.style.margin = "0 0 10px";
                 }
@@ -133,7 +136,7 @@ function showStamps() {
                 percentText = document.getElementById('percentage');
                 percentText.innerHTML = `${roundedDown}%`;
                 // Style the bar
-                filled = document.getElementById("filled");
+                var filled = document.getElementById("filled");
                 filled.style.backgroundColor = color;
                 filled.style.width = percentageCompletion + "%";
             }
@@ -166,10 +169,10 @@ function stampsOverlay() {
             if ('stamps' in data) {
                 var stamps = data.stamps;
                 // Makes overlay appear
-                overlay = document.getElementById("stampsOverlay");
+                var overlay = document.getElementById("stampsOverlay");
                 overlay.style.display = 'flex';
                 // Makes text for stamps appear
-                stampsText = document.getElementById("stampsText");
+                var stampsText = document.getElementById("stampsText");
                 stampsText.innerHTML = `${stamps} stamps`;
             }
         }
@@ -224,11 +227,197 @@ function logout() {
 function logoutModal() {
     document.getElementById('logoutModal').style.display = 'flex';
     document.addEventListener('click', function(event) {
-        modalContent = document.getElementById("modalContent");
+        var modalContent = document.getElementById("modalContent");
         var targetElement = event.target;
         // Check if click happened outside the box
         if (targetElement != modalContent && !modalContent.contains(targetElement)) {
             cancelLogout();
         }
+    });
+}
+
+// Will put an overlay asking you if you want to exit 
+function closeModalConf() {
+    document.getElementById('confirmClose').style.display = 'flex';
+}
+
+// Will close the modal that confirms closing
+function stayOnCurrentScreen() {
+    document.getElementById('confirmClose').style.display = 'none';
+}
+
+function closePassModal() {
+    // Remove values from all input fields
+    document.getElementById('old').value = '';
+    document.getElementById('new').value = '';
+    document.getElementById('conf').value = '';
+    // Remove display on modal
+    document.getElementById('passwordModal').style.display = 'none';
+    document.getElementById('passwordResponse').innerHTML = '';
+    // Remove display on confirmation modal
+    document.getElementById('confirmClose').style.display = 'none';
+}
+
+function showPasswordModal() {
+    var passwordModal = document.getElementById('passwordModal');
+    passwordModal.style.display = 'flex';
+    passwordModal.addEventListener('click', function(event) {
+        var modalContent = document.getElementById('passwordContainer');
+        var targetElement = event.target;
+        if (targetElement != modalContent && !modalContent.contains(targetElement)) {
+            closeModalConf();
+        }
+    });
+    const passwordForm = document.getElementById("new");
+    const advice = document.getElementById("passwordStrength");
+    const confirmInput = document.getElementById("conf");
+    const confirmLabel = document.getElementById("confLabel");
+
+    var response = '';
+    response += "<span class='red'>&#x2715; Lowercase<br>"; // &#x2715; is cross
+    response += "&#x2715; Uppercase<br>";
+    response += "&#x2715; Number<br>";
+    response += "&#x2715; Symbol<br>";
+    response += `&#x2715; ${minChars}-${maxChars} Characters<br></span>`;
+    advice.innerHTML = response;
+
+    document.addEventListener('input', function() {
+        // cross is &#x2715;, tick is &checkmark;
+        response = '';
+        if (passwordForm.value) {
+            var password = passwordForm.value;
+            // removes non-ASCII so no emojis in your password :)
+            password.replace(/[a-zA-Z0-9!@£#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, "");
+            passwordForm.value = password;
+        }
+        else {
+            var password = '';
+        }
+        // RegEx
+        if (password.match(/[a-z]/, password)) {
+            response += "<span class='green'>&checkmark; Lowercase</span><br>";
+            var hasLower = true;
+        }
+        else {
+            response += "<span class='red'>&#x2715; Lowercase</span><br>";
+            var hasLower = false;
+        }
+        if (password.match(/[A-Z]/, password)) {
+            response += "<span class='green'>&checkmark; Uppercase</span><br>";
+            var hasUpper = true;
+        }
+        else {
+            response += "<span class='red'>&#x2715; Uppercase</span><br>";
+            var hasUpper = false;
+        }
+        if (password.match(/[0-9]/, password)) {
+            response += "<span class='green'>&checkmark; Number</span><br>";
+            var hasNum = true;
+        }
+        else {
+            response += "<span class='red'>&#x2715; Number</span><br>";
+            var hasNum = false;
+        }
+        if (password.match(/[!@£#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, password)) {
+            response += "<span class='green'>&checkmark; Symbol</span><br>";
+            var hasSym = true;
+        }
+        else {
+            response += "<span class='red'>&#x2715; Symbol</span><br>";
+            var hasSym = false;
+        }
+        if (password.length >= minChars && password.length <= maxChars) {
+            response += `<span class='green'>&checkmark; ${minChars}-${maxChars} Characters</span><br>`;
+            var goodLength = true;
+        }
+        else {
+            response += `<span class='red'>&#x2715; ${minChars}-${maxChars} Characters</span><br>`;
+            var goodLength = false;
+        }
+        if (hasLower && hasUpper && hasNum && hasSym && goodLength) {
+            confirmLabel.style.display = 'flex';
+            confirmInput.style.display = 'flex';
+            if (confirmInput.value && confirmInput.value === password) {
+                response += "<span class='green'>&checkmark; Passwords Match</span>";
+                document.getElementById('submit').style.display = 'block';
+            }
+            else {
+                response += "<span class='red'>&#x2715; Passwords Match</span>";
+                document.getElementById('submit').style.display = 'none';
+            }
+        }
+        else {
+            confirmLabel.style.display = 'none';
+            confirmInput.style.display = 'none';
+            confirmInput.value = '';
+        }
+        advice.innerHTML = response;
     })
+}
+
+function submitForm() {
+    var oldField = document.getElementById("old");
+    var newField = document.getElementById("new");
+    var confField = document.getElementById("conf");
+    var responseElement = document.getElementById("passwordResponse");
+    // Check whether fields are truthy
+    if (oldField.value && newField.value && confField.value) {
+        var oldPass = oldField.value;
+        var newPass = newField.value;
+        var confPass = confField.value;
+        if (newPass === confPass) {
+            fetch("http://localhost/digistamp/dashboard.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "old": oldPass,
+                    "new": newPass,
+                    "confirm": confPass
+                })
+            })
+            .then (response => {
+                if (!response.ok) {
+                    document.getElementById("passwordResponse").innerHTML = "Failed to get a response";
+                    console.error("Network response was not ok.")
+                }
+                isJson = response.headers.get('Content-Type').includes('application/json');
+                if (isJson) {
+                    return response.json();
+                }
+            })
+            .then(data => {
+                var fullOpacity = document.getElementsByClassName('fullOpacity')[0];
+                fullOpacity.style.transition = 'none';
+                responseElement.classList.remove('fadeout');
+                if (typeof data === "object" && data !== null) {
+                    if ('success' in data) {
+                        responseElement.innerHTML = data.success;
+                    }
+                    else if ('failure' in data) {
+                        responseElement.innerHTML = data.failure;
+                    }
+                    else {
+                        responseElement.innerHTML = "Failed to get a response";
+                    }
+                    setTimeout(function() {
+                        responseElement.style.transition = 'opacity 3s ease';
+                        responseElement.classList.add('fadeout');
+                        responseElement.addEventListener('transitionend', function(event) {
+                        if (event.propertyName === 'opacity') {
+                            responseElement.innerHTML = '';
+                            if ('success' in data) {
+
+                            }
+                        }
+                        });
+                    }, 4000);
+                }
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+        }
+    }
 }
