f81f69bfaff0573a0e7033a8e9211d5e25fa26d8
userinit <93477626+userinit@users.noreply.github.com>
2024-02-26 22:48:28 +0000

    Added 'danger zone' with admin functionalities

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,14 @@ $srvuser = "root";
 $srvpass = "";
 $db = "schoolrewardsdb";
 
+// Class array for updating classes
+$yearInfo = [
+    "10" => ["Freud", "Galilei", "Lavoisier", "Kepler", "Copernicus"],
+    "11" => ["Pauling", "Virchow", "Schrodinger", "Rutherford", "Dirac"],
+    "12" => ["Curie", "Herschel", "Lyell", "Laplace", "Hubble"],
+    "13" => ["Eddington", "Harvey", "Malpighi", "Huygens", "Gauss"]
+];
+
 // For password reset
 $minChars = 10; // Edit this depending on password requirements
 $maxChars = 30; // Max chars to stop overflows
@@ -231,10 +239,126 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
                 header("Content-Type: application/json");
                 echo $failure;
             }
+            if (isset($conn)) {
+                $conn->close();
+                $stmt->close();
+            }
         }
-        if (isset($conn)) {
-            $conn->close();
-            $stmt->close();
+        elseif (isset($_SESSION['role']) && $_SESSION['role'] === "admin") {
+            if (array_key_exists("moveYearUp", $decoded) && $decoded['moveYearUp'] === TRUE) {
+                $conn = new mysqli($host, $srvuser, $srvpass, $db);
+                if ($conn->connect_error) {
+                    die("Error: " . $conn->connect_error);
+                }
+                $result1 = $conn->query("DELETE * FROM students WHERE school_year = 11 OR school_year = 13;");
+                $result2 = $conn->query("UPDATE students SET school_year = 11 WHERE school_year = 10;");
+                $result3 = $conn->query("UPDATE students SET school_year = 13 WHERE school_year = 12;");
+    
+                if ($result1 && $result2 && $result3) {
+                    $json = json_encode(array("Removed year 11 and 13. Moved year 10->11. Moved year 12->13"));
+                    header("Content-Type: application/json");
+                    echo $json;
+                    // All options have been processed, send response   
+                }
+                else {
+                    $failMsg = '';
+                    if (!$result1) {
+                        $failMsg .= "Failed to remove year 11 and 13. ";
+                    }
+                    if (!$result2) {
+                        $failMsg .= "Failed to move year 10->11. ";
+                    }
+                    if (!$result3) {
+                        $failMsg .= "Failed to move year 12->13. ";
+                    }
+                    $json = json_encode(array("failure" => $failMsg));
+                    header("Content-Type: application/json");
+                    echo $json;
+                }
+            }
+            if (isset($_FILES['csvFile'])) {
+                $csvFile = $_FILES['csvFile']['tmp_name'];
+                $handle = fopen($csvFile, "r");
+                if ($handle !== FALSE) {
+                    $firstRow = fgetcsv($handle);
+                    $firstRow = array_map('strtolower', $firstRow);
+                    $firstRow = array_map('trim', $firstRow);
+                    $firstRowCols = count($firstRow);
+                    if ($firstRowCols === 2) {
+                        $usernameIndex = array_search('username');
+                        $classIndex = array_search('class');
+                        if ($usernameIndex === FALSE || $classIndex === FALSE) {
+                            // handle
+                        }
+                        else {
+                            $f = 0; // $f is failure index for key-value pairs
+                            $s = 0; // $s is success index for key-value pairs
+                            while (($data = fgetcsv($handle)) !== FALSE) {
+                                $username = $data[$usernameIndex];
+                                $class = $data[$classIndex];
+                                $conn = new mysqli($host, $srvuser, $srvpass, $db);
+                                if ($conn->connect_error) {
+                                    die("Error: " . $conn->connect_error);
+                                }
+                                $stmt = $conn->prepare("SELECT * FROM students WHERE username = ?;");
+                                $stmt->bind_param("s", $username);
+                                $stmt->execute();
+                                if (!($stmt->get_result()->num_rows > 0 )) {
+                                    $failure[] = array("Failure$f" => [
+                                        "success" => "No",
+                                        "username" => $username,
+                                        "fullname" => "",
+                                        "startClass" => "",
+                                        "endClass" => $class,
+                                        "errors" => "Invalid username"
+                                    ]);
+                                    $f++;
+                                }
+                                else {
+                                    $results = $stmt->get_result()->fetch_assoc();
+                                    $year = $results['school_year'];
+                                    $startClass = $results['class'];
+                                    $forename = $results['forename'];
+                                    $surname = $results['surname'];
+                                    $fullname = "$forename $surname";
+                                    $validClass = array_search($class, $yearInfo[$year]);
+                                    if (!($validClass === TRUE)) {
+                                        $failure[] = array("Failure$f" => [
+                                            "success" => "No",
+                                            "username" => $username,
+                                            "fullname" => $fullname,
+                                            "startClass" => $startClass,
+                                            "endClass" => $class,
+                                            "errors" => "Invalid new class"
+                                        ]);
+                                        $f++;
+                                    }
+                                    else {
+                                        // valid class - continue SQL logic
+                                        $stmt = $conn->prepare("UPDATE students SET class = ? WHERE username = ?;");
+                                        $stmt->bind_param("ss", $class, $username);
+                                        $stmt->execute();
+                                        if ($stmt->affected_rows > 0) {
+                                            $success[] = array("Success$f" => [
+                                                "success" => "Yes",
+                                                "username" => $username,
+                                                "fullname" => $fullname,
+                                                "startClass" => $startClass,
+                                                "endClass" => $class,
+                                                "errors" => "N/A"
+                                            ]);
+                                            $s++;
+                                        }
+                                    }
+                                }
+                            }
+                            $jsonArr = json_encode(array($success, $failure));
+                            header("Content-Type: application/json");
+                            echo $jsonArr;
+                        }
+                    }
+                }
+            }
         }
     }
 }
